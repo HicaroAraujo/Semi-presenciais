b428b071935114014f09ff0680ce3595e7126be9
Hicaro Araujo <164560459+HicaroAraujo@users.noreply.github.com>
2024-11-28 11:21:29 -0300

    alteração de acesso as rotas

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,31 @@ use App\Http\Controllers\TimeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('time', [TimeController::class, 'index'])
-;
-Route::get('time/create', [TimeController::class, 'create']);
-Route::post('time', [TimeController::class, 'store']);
-Route::get('time/{id}/edit', [TimeController::class, 'edit']);
-Route::put('time/{id}', [TimeController::class, 'update']);
-Route::delete('time/{id}', [TimeController::class, 'destroy']);
+->middleware(['auth', 'verified'])->name('index-time');
+Route::get('time/create', [TimeController::class, 'create'])
+->middleware(['auth', 'verified'])->name('create-time');
+Route::post('time', [TimeController::class, 'store'])
+->middleware(['auth', 'verified'])->name('store-time');
+Route::get('time/{id}/edit', [TimeController::class, 'edit'])
+->middleware(['auth', 'verified'])->name('edit-time');
+Route::put('time/{id}', [TimeController::class, 'update'])
+->middleware(['auth', 'verified'])->name('update-time');
+Route::delete('time/{id}', [TimeController::class, 'destroy'])
+->middleware(['auth', 'verified'])->name('destroy-time');
 
 
-Route::get('coaches', [CoachController::class, 'index']);
-Route::get('coaches/create', [CoachController::class, 'create']);
-Route::post('coaches', [CoachController::class, 'store']);
-Route::get('coaches/{id}/edit', [CoachController::class, 'edit']);
-Route::put('coaches/{id}', [CoachController::class, 'update']);
-Route::delete('coaches/{id}', [CoachController::class, 'destroy']);
+Route::get('coaches', [CoachController::class, 'index'])
+->middleware(['auth', 'verified'])->name('index-coaches');
+Route::get('coaches/create', [CoachController::class, 'create'])
+->middleware(['auth', 'verified'])->name('create-coaches');
+Route::post('coaches', [CoachController::class, 'store'])
+->middleware(['auth', 'verified'])->name('store-coaches');
+Route::get('coaches/{id}/edit', [CoachController::class, 'edit'])
+->middleware(['auth', 'verified'])->name('edit-coaches');
+Route::put('coaches/{id}', [CoachController::class, 'update'])
+->middleware(['auth', 'verified'])->name('update-coaches');
+Route::delete('coaches/{id}', [CoachController::class, 'destroy'])
+->middleware(['auth', 'verified'])->name('destroy-coaches');
 
 Route::get('/', function () {
     return view('welcome');
